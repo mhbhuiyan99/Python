bc30834d14885208bd5aead5fb4ae935333bae58
Mojammel Haque Bhuiyan <98250129+mhbhuiyan99@users.noreply.github.com>
2026-06-11 10:48:38 +0600

    Update and rename TASK: Loop.md to TASK: Loop.py

---
+++ 02: Loops/TASK: Loop.py
@@ -0,0 +1,22 @@
+# TASK 2A: FizzBuzz (Classic interview question)
+# Print 1 to 50:
+# - "Fizz" if divisible by 3
+# - "Buzz" if divisible by 5  
+# - "FizzBuzz" if divisible by both
+# - Number otherwise
+
+# TASK 2B: Given two lists, find common elements WITHOUT using sets
+list1 = [1, 2, 3, 4, 5, 6]
+list2 = [4, 5, 6, 7, 8, 9]
+# Expected: [4, 5, 6]
+
+# TASK 2C: Print this pattern
+# *
+# **
+# ***
+# ****
+# *****
+
+# TASK 2D: Flatten a nested list
+nested = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+# Expected: [1, 2, 3, 4, 5, 6, 7, 8, 9]
